120fc26d00a163094048bec5addc6b6b3aa6a95e
ArtemDzhek <108084556+ArtemDzhek@users.noreply.github.com>
2022-12-27 21:58:11 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,454 @@
+"use strict"
+
+
+// console.log('35' + -"22");
+
+// let coMbo = ('558' > 557);
+// console.log(coMbo);
+
+// console.log('35' * "22");
+
+// console.log('558' > 22++);
+
+// let usersCounter = 0;
+// let newUsers = usersCounter++;
+// console.log(newUsers);
+
+
+// console.log(!false && 11 || 18 && ! '');
+
+// let name = 0;
+// console.log(name ?? 'без імені');
+// let toMbo = 1;
+// let coMbo = 5;
+// console.log(coMbo);
+// coMbo = coMbo + toMbo;
+// console.log(coMbo);
+// let cocoMbo = 7 === 2;
+// console.log(cocoMbo);
+// cocoMbo = null;
+// cocoMbo = 0;
+// cocoMbo = '';
+// let cocoMbo = null >= 0;
+// console.log(cocoMbo);
+
+// let userName = '';
+// let userNickName = '';
+// let user = userName || userNickName || "doe";
+// console.log(user);
+
+// let coMbo = 9;
+// let toMbo = null;
+// let cocoMbo = 8;
+// console.log(coMbo && toMbo && cocoMbo);
+
+// let coMbo = null;
+// console.log(coMbo ?? 'kcukarazha');
+
+
+// if (1 === "1") {
+//    console.log('Правда!');
+// } else {
+//    console.log('Брехня!');
+// }
+
+// if (5 == "5") {
+//    console.log('Правда!');
+// } else {
+//    console.log('Брехня!');
+// }
+
+// let message = (92 > '11' && 58 < 100) ? 'Правда!' : 'Брехня!';
+// console.log(message);
+
+// if (0) {
+//    console.log('Брехня!');
+// } else if (" ") {
+//    console.log('Правда!');
+// }
+// let moMbo = "not hi"
+// let toMbo = "Hi!"
+// let first = 5;
+// let second = 7;
+
+// if (2 > 1) {
+//    console.log(toMbo);
+// }
+// if (first !== second) {
+//    console.log(toMbo);
+// }
+
+// if (2 + 1 === 3 && "1" || 10 > 5 && 10 === 1) {
+//    console.log(moMbo);
+// }
+
+// if ("0") {
+//    console.log(toMbo);
+// }
+
+// if (2 > 1) console.log(moMbo);
+
+// if (first > 3) {
+//    console.log(toMbo);
+// } else {
+//    console.log(moMbo);
+// }
+
+// if (first > 10) {
+//    console.log(toMbo);
+// } else if (first > 20) {
+//    console.log(moMbo);
+// } else if (first > 30) {
+//    console.log(moMbo);
+// } else if (first <= 40) {
+//    console.log(toMbo);
+// } else if (first < 50) {
+//    console.log(toMbo);
+// }
+
+// let messageSecond = "How are you?"
+// let message = 4 > 6 ? toMbo : moMbo;
+// console.log(message);
+
+// messageSecond += message;
+// console.log(messageSecond);
+
+
+// let num = 8;
+// while (num) {
+//    console.log(num);
+//    num--;
+// }
+
+// for (let num = 0; num < 3; num++) {
+
+//    console.log(`Number: ${num}`);
+// }
+
+// let num = 0;
+// while (num < 3) {
+//    num++;
+//    console.log(`Number: ${num}`);
+// }
+
+
+
+// toMbo: for (let num = 0; num < 2; num++) {
+
+//    for (let size = 0; size < 3; size++) {
+//       if (size == 2) {
+//          break toMbo;
+//       }
+//       console.log(size);
+//    }
+
+// }
+
+// let num = 0;
+// while (num < 5) {
+//    console.log(num);
+//    num++;
+// }
+
+// let num = 5;
+// while (num) {
+//    console.log(num);
+//    num--;
+// }
+
+// let num = 0;
+// do {
+//    console.log(num);
+//    num++;
+// } while (num < 5);
+
+// for (let num = 0; num < 5; num++) {
+//    console.log(num);
+// }
+
+// let num = 0;
+// for (; num < 5; num++) {
+//    console.timeLog(num);
+//    if (num >= 3) break;
+// }
+// console.log(`праця завершена, num = ${num} `)
+
+// let num = 0;
+// for (; num < 5; num++) {
+//    if (num == 2) continue;
+//    console.log(num);
+// }
+
+// firstFor: for (let num = 0; num < 2; num++) {
+//    for (let size = 0; size < 6; size++) {
+//       if (size == 6) {
+//          continue firstFor;
+//       }
+//       console.log(size)
+//    }
+// }
+
+
+// let num = 1;
+// while (num <= 5) {
+//    console.log(num);
+//    num++;
+// }
+
+// let num = 5;
+// while (num) {
+//    console.log(num);
+//    num--;
+// }
+
+// let num = 1;
+// do {
+//    console.log(num);
+//    num++;
+// } while (num < 6);
+
+// for (let num = 1; num <= 5; num++) {
+//    console.log(num);
+// }
+
+// let num = 1;
+// for (; num < 10; num++) {
+//    console.log(num);
+//    if (num === 5) break;
+// }
+// console.log(`мабуть вистачить num = ${num}`);
+
+// let num = 1;
+// for (; num < 10; num++) {
+//    if (num === 6) continue;
+//    if (num === 7) continue;
+//    if (num === 8) continue;
+//    if (num === 9) continue;
+//    if (num === 10) continue;
+//    console.log(num);
+// }
+
+//=============
+// let numOne = 1.005 + Number.EPSILON;
+// numOne = Math.round(numOne * 100) / 100;
+// console.log(numOne);
+
+// let value = "135.58px";
+
+// let value = parseFloat("150.58px");
+// console.log(value);
+
+
+// let value = 58 + "Фрілансер";
+// if (value !== NaN) {
+//    console.log('Результат виразу NaN');
+// }
+
+
+
+
+// console.log(Math.max(10, 58, 39, -150, 0));
+
+
+// let numOne = Math.floor(58.858);
+// console.log(numOne.toFixed());
+//=============
+
+// let someNum = 458;
+// console.log(someNum);
+
+// let someNum = 4.58;
+// console.log(someNum)
+
+// let someNum = 1e6;
+// console.log(someNum);
+
+// let someNum = 1e-6;
+// console.log(someNum);
+
+// console.log(0xff);
+// console.log(0b11111111);
+// console.log(0o377);
+
+// let num = 255;
+// console.log(num.toString(16));
+// console.log(num.toString(8));
+// console.log(num.toString(2));
+
+// let numOne = Math.floor(5.8);
+// let numTuo = Math.floor(2.2);
+// let numThree = Math.floor(-2.2);
+
+// console.log(numOne);
+// console.log(numTuo);
+// console.log(numThree);
+
+// let numOne = Math.ceil(5.8);
+// let numTuo = Math.ceil(2.2);
+// let numThree = Math.ceil(-2.2);
+
+// console.log(numOne);
+// console.log(numTuo);
+// console.log(numThree);
+
+// let numOne = Math.round(5.8);
+// let numTuo = Math.round(2.2);
+// let numThree = Math.round(-2.2);
+
+// console.log(numOne);
+// console.log(numTuo);
+// console.log(numThree);
+
+// let numOne = Math.round(5.845 * 10) / 10;
+// console.log(numOne);
+
+// let numTuo = Math.round(5.845 * 100) / 100;
+// console.log(numTuo);
+
+// let numThree = Math.round(5.8449 * 100) / 100;
+// console.log(numThree);
+
+// let numOne = 5.845;
+// console.log(numOne.toFixed(1));
+// console.log(+numOne.toFixed(1));
+// console.log(Number(numOne.toFixed(1)));
+
+// let numOne = Math.round(1.005 * 100) / 100;
+// console.log(numOne);
+
+// let numTuo = 12.35;
+// console.log(numTuo.toFixed(1));
+
+// let numOne = (1.005 * 100) / 100;
+// console.log(numOne.toFixed(3));
+
+// let numTuo = Math.ceil(12.35 * 10) / 10;
+// console.log(numTuo);
+
+// let problem = 0.1 + 0.2 === 0.3;
+// console.log(problem);
+
+// let toMbo = 2;
+// let moMbo = 3;
+// let coMbo = (toMbo + moMbo) === 5;
+// console.log(coMbo);
+
+// let sourceNum = 1.005 + Number.EPSILON;
+// let numFour = Math.round(sourceNum * 100) / 100;
+// console.log(numFour);
+
+// console.log(sourceNum * 100);
+// console.log(Math.round(sourceNum * 100));
+
+// console.log(Number(25 + "Вітаю!"));
+// console.log(isNaN(25 + "Вітаю!"));
+
+// console.log(isFinite("25"));
+// console.log(isFinite("Вітаю!"));
+// console.log(isFinite(10 / 0));
+
+// let valueOne = +"150";
+// console.log(valueOne);
+// console.log(typeof valueOne);
+
+// let valueTwo = +"150px";
+// console.log(valueTwo);
+// console.log(typeof valueTwo);
+
+// let valueOne = parseInt("150.58px");
+// console.log(valueOne);
+// console.log(typeof valueOne);
+
+// let valueTwo = parseFloat("150.58px");
+// console.log(valueTwo);
+// console.log(typeof valueTwo);
+
+// console.log(parseInt('0xff', 16));
+
+// console.log(parseInt('ff', 16));
+
+// console.log(Math.max(5, 85, -49));
+// console.log(Math.min(5, 85, -49));
+
+// let toMbo = (25 - 40);
+// console.log(Math.abs(toMbo));
+
+
+//===========================
+
+// let fis = "Фрілансер";
+// let text = (`Привіт я ${fis}`);
+// console.log(text);
+
+// let text = 'Фрілансер';
+// console.log(text.slice(5, 6));
+
+// let text = 123 + "456";
+// console.log(text);
+
+
+// let text = "Фрілансер";
+// console.log(text.toUpperCase());
+
+// let text = "Фрілансер";
+// console.log(text.slice(3, 6));
+
+// let text = "Фрілансер";
+// console.log(text.includes('лан', 4));
+
+//============================
+
+// let textHi = "Привіт!";
+// let textAll = `${textHi} я строка.`
+// console.log(textAll);
+
+// function someSum(a, b) {
+//    return a + b;
+// }
+// console.log(`Сумма ${someSum(4, 7)}`);
+
+// let textOne = "Привіт\n ви\n\t на каналі\n\t\t Фрилансер по\n\t\t життю!";
+// console.log(textOne);
+// console.log(textOne.length);
+
+// let firstSymbol = textOne[0];
+// let lastSymbol = textOne[textOne.length - 22];
+// console.log(firstSymbol);
+// console.log(lastSymbol);
+
+// for (const char of textOne) {
+//    console.log(char);
+// }
+
+// let text = "Привіт!";
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+
+
+// let text = "Привіт!";
+// console.log(text.indexOf('рив'));
+// console.log(text.indexOf('іт', 5));
+
+// console.log(text.includes('рив'));
+// console.log(text.includes('іт', 5));
+
+// console.log(text.startsWith('При'));
+// console.log(text.endsWith('іт'));
+// let searchText = "пР";
+// console.log(text.toLowerCase().includes(searchText.toLowerCase()));
+
+// let text = "Привіт ви на каналі Фрилансер по життю!";
+// console.log(text.slice(0, 3));
+// console.log(text.slice(-6, -1));
+// console.log(text.slice(1));
+
+// let text = 'Привіт ви на каналі Фрилансер по життю!';
+// text = text.replace('Привіт', 'Вітаю,');
+// console.log(text);
+
+// let text = 'Привіт ви на каналі Фрилансер по життю! каналі Фрилансер';
+// console.log(`кількість символів: ${text.length}`);
+
+// for (let i = 0; i < text.length; ++i) {
+//    console.log(text[i]);
+// }
